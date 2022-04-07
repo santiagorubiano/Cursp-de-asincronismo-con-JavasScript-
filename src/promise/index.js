e8758e57ede1c266275d57b3ea268deb58bbfae5
santiago <santiago.rubianom@gmail.com>
2022-04-07 16:32:09 -0500

    promesas ejemplos terminados

--- src/promise/index.js
+++ src/promise/index.js
@@ -17,7 +17,7 @@ somethingWillHappen()
 
 const somethingWillHappen2 = () =>{
     return new Promise((resolve,reject)=>{
-        if(false){
+        if(true){
             setTimeout(() => {
                 resolve('True')
             }, 2000);
@@ -30,3 +30,11 @@ const somethingWillHappen2 = () =>{
 somethingWillHappen2()
 .then(response=> console.log(response))
 .catch(err => console.error(err));
+
+Promise.all([somethingWillHappen(),somethingWillHappen2()])
+.then(response=> {
+    console.log('Array of results', response)
+})
+.catch(err=>{
+    console.error(err);
+})
